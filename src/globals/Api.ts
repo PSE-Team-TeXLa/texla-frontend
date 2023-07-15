724eac86569ad833b31e15f99193fa2ffce484e6
linus.schoeb <linus.schoeb@student.kit.edu>
2023-07-15 21:46:32 +0200

    Use socket.io (connecting works)

--- src/globals/Api.ts
+++ src/globals/Api.ts
@@ -1,4 +1,17 @@
+// import { io } from "../lib/socket.io.esm.min";
+import { io } from "socket.io-client";
+
+const socket = io("ws://localhost:13814/");
+console.count("socket established");
+// of course, it should only be one
+
 export function editNode(target: bigint, raw_latex: string) {
-    console.log(target);
-    console.log(raw_latex);
+    // TODO: Use API Types
+    const operation = {
+        target: Number(target), // TODO: information is lost here! -> limit uuids to 2^53 or 2^32
+        raw_latex
+    }
+    
+    socket.emit("operation", operation);
+    console.log("operation sent: ", operation);
 }
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
     import "../app.css";
     import Sidebar from "../components/sidebar/Sidebar.svelte";
+    import "../globals/Api"
 </script>
 
 
