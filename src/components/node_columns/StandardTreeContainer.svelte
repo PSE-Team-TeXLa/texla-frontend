cd30f935dc830ec4c53f3ba7e29d4c32dc56e16b
linus.schoeb <linus.schoeb@student.kit.edu>
2023-07-18 23:34:31 +0200

    Make json_ast optional

--- src/components/node_columns/StandardTreeContainer.svelte
+++ src/components/node_columns/StandardTreeContainer.svelte
@@ -20,14 +20,16 @@
 
 <div class="snap-proximity snap-y h-full w-full p-10 overflow-scroll overflow-x-hidden">
 
-    {#if isNavColumn}
+    {#if $json_ast !== undefined}
+        {#if isNavColumn}
             <!--<StandardDocumentNode uuid={$json_ast.root.uuid} filename={$json_ast.root.node_type.data.filename} children={$json_ast.root.node_type.children} layerShown={layerShown} isNavColumn="{isNavColumn}" />-->
             <StandardDocumentNode bind:node={$json_ast.root} layerShown={layerShown} isNavColumn="{isNavColumn}"/>
-    {:else}
-        <div bind:this={container}>
-            <StandardDocumentNode bind:node={$json_ast.root} layerShown={layerShown} isNavColumn="{isNavColumn}"/>
-            <!--<StandardDocumentNode uuid={$json_ast.root.uuid} filename={$json_ast.root.node_type.data.filename} children={$json_ast.root.node_type.children} layerShown={layerShown} isNavColumn="{isNavColumn}" />-->
-        </div>
+        {:else}
+            <div bind:this={container}>
+                <StandardDocumentNode bind:node={$json_ast.root} layerShown={layerShown} isNavColumn="{isNavColumn}"/>
+                <!--<StandardDocumentNode uuid={$json_ast.root.uuid} filename={$json_ast.root.node_type.data.filename} children={$json_ast.root.node_type.children} layerShown={layerShown} isNavColumn="{isNavColumn}" />-->
+            </div>
+        {/if}
     {/if}
 </div>
 
