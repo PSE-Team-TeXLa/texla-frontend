b408950c070fd2f85450333dd473bd239c868f46
linus.schoeb <linus.schoeb@student.kit.edu>
2023-07-27 13:01:22 +0200

    Handle errors in rendering

--- src/components/rendering/Latex.svelte
+++ src/components/rendering/Latex.svelte
@@ -4,13 +4,30 @@
     declare const latexjs: any;
     
     export let latex: string = "";
-
-    let generator = new latexjs.HtmlGenerator({
-        styles: ["css/custom.css"]
-    });
-    generator = latexjs.parse(latex, {generator});
-    const doc = generator.htmlDocument() as Document;
-    const html = doc.children[0].outerHTML;
+    
+    // error handling strategy:
+    // try rendering it
+    // try rendering it with backslashes escaped
+    // fallback: display latex as code
+    let html: string;
+    try {
+        let generator = new latexjs.HtmlGenerator({
+            styles: ["css/custom.css"]
+        });
+        generator = latexjs.parse(latex, {generator});
+        const doc = generator.htmlDocument() as Document;
+        html = doc.children[0].outerHTML;
+    } catch (e) {
+        try {
+            latex = latex.replaceAll("\\", "\\textbackslash{}");
+            let generator = new latexjs.HtmlGenerator({
+                styles: ["css/custom.css"]
+            });
+            generator = latexjs.parse(latex, {generator});
+            const doc = generator.htmlDocument() as Document;
+            html = doc.children[0].outerHTML;        
+        } catch (e) {}
+    }
 
     // iframe takes 150px even if the content is much smaller -> we have to resize it to fit content
     let height = 0;
@@ -22,7 +39,13 @@
 </script>
 
 <div class="relative">
-    <iframe class="inline-block w-full" title="latex-snippet" sandbox="allow-same-origin allow-scripts" srcdoc="{html}"
-            on:load={resizeIframe} height="{height}"></iframe>
-    <div class="facade absolute top-0 w-full z-50" style="height: {height}px;"></div>
+    {#if html}
+        <iframe class="inline-block w-full" title="latex-snippet" sandbox="allow-same-origin allow-scripts" srcdoc="{html}"
+                on:load={resizeIframe} height="{height}"></iframe>
+        <div class="facade absolute top-0 w-full z-50" style="height: {height}px;"></div>
+    {:else}
+        <code class="text-[16px]">
+            {latex}
+        </code>
+    {/if}
 </div>
