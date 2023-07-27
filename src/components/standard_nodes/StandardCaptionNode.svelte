8192597d8aac90b334b7b418633b1528ee569782
piotr.malkowski <piotr.malkowski@student.kit.edu>
2023-07-27 15:32:41 +0200

    Add placeholder divs in leaf components

---
+++ src/components/standard_nodes/StandardCaptionNode.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+
+  import StandardLeafNode from "./StandardLeafNode.svelte";
+  import API from "../../globals/socket.api.d.ts";
+
+  export let parent;
+  export let node: API.Ast.Node;
+
+  //TODO test after Tutorium (change div placeholder), change input Latex File in backend.
+</script>
+
+
+
+<StandardLeafNode parent={parent} bind:node>
+  <div class="flex flex-col cursor-default my-2">
+    <span class="whitespace-pre-wrap text-lg">{node.node_type.data.caption}</span>
+  </div>
+</StandardLeafNode>
