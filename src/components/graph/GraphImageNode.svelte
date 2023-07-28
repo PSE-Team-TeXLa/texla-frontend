61aa35732748757e2bdcfcd1e56587ed250e8e16
linus.schoeb <linus.schoeb@student.kit.edu>
2023-07-28 20:10:15 +0200

    Adopt url changes in GraphImageNode

--- src/components/graph/GraphImageNode.svelte
+++ src/components/graph/GraphImageNode.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
+    import {backendUrl} from "../../globals/Constants";
     import type API from "../../globals/socket.api";
     import GraphLeafNode from "./GraphLeafNode.svelte";
 
     export let node: API.Ast.Node<API.Ast.LeafType<API.Ast.Image>>;
+
+    const src = `${backendUrl}user-assets/${node.node_type.data.path}`;
 </script>
 <GraphLeafNode>
     <div class="flex justify-center items-center">
-        <img src={node.node_type.data.path} alt={node.node_type.data.path} class="w-96 h-auto"/>
+        <img {src} alt={node.node_type.data.path} class="max-h-20 h-auto"/>
     </div>
 </GraphLeafNode>
