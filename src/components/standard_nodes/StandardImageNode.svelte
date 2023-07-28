8764c61d45b52683356c84b0c90405d5777f329b
linus.schoeb <linus.schoeb@student.kit.edu>
2023-07-28 13:31:24 +0200

    Request images from user-assets

--- src/components/standard_nodes/StandardImageNode.svelte
+++ src/components/standard_nodes/StandardImageNode.svelte
@@ -2,15 +2,16 @@
     import StandardLeafNode from "./StandardLeafNode.svelte";
     import type API from "../../globals/socket.api.d.ts";
 
-    export let node: API.Ast.Node;
+    export let node: API.Ast.Node<API.Ast.LeafType<API.Ast.Image>>;
     export let parent;
 
+    // TODO: canonicalize path
 </script>
 
 
 <StandardLeafNode parent={parent} bind:node>
     <div class="my-4 flex flex-col items-center">
-        <img alt={node.node_type.data.path} src={node.node_type.data.path} class="max-h-20 w-auto"/>
+        <img alt={node.node_type.data.path} src={"/user-assets/" + node.node_type.data.path} class="max-h-20 w-auto"/>
     </div>
 </StandardLeafNode>
 
