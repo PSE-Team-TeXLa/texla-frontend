01daa6cd06b3d34dbd941c5143835bfb285f6956
linus.schoeb <linus.schoeb@student.kit.edu>
2023-08-23 16:33:47 +0200

    Render math environments using $$

--- src/components/nodes/standard_nodes/StandardMathNode.svelte
+++ src/components/nodes/standard_nodes/StandardMathNode.svelte
@@ -5,12 +5,18 @@
 
     export let parent;
     export let node: API.Ast.Node<API.Ast.LeafType<API.Ast.Math>>;
+
+    // latex.js can only render $...$ and $$...$$ math, but no other math-environments
+    // -> we convert from them to $$...$$
+    let latex = node.node_type.data.content;
+    latex = latex.replaceAll("&", "");
+    latex = `$$${latex}$$`;
 </script>
 
 <StandardLeafNode parent={parent} bind:node>
     <div class="flex flex-col cursor-default my-2">
         <span class="text-lg">
-            <Latex latex="${node.node_type.data.content}$"></Latex>
+            <Latex {latex}></Latex>
         </span>
     </div>
 </StandardLeafNode>
